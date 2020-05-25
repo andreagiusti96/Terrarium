3f2cd3304c9e7f881c5f7134cb9d0138156aacdd
Giancarlo Maffettone <giancarlo@iMacdiGiancarlo.homenet.telecomitalia.it>
2020-05-25 09:43:16 +0200

    ChickensFoxesEvOff

--- Terrarium/Assets/Scripts/CreatureBehaviour/AsexualCommonDuplication.cs
+++ Terrarium/Assets/Scripts/CreatureBehaviour/AsexualCommonDuplication.cs
@@ -18,10 +18,18 @@ namespace Assets.Scripts.CreatureBehaviour
              * This is a demo of how you may finetune your newborn
              * As you may, see mutation happens here !
              */
+            //Evolution On
+            //baby.CreatureRegime = parent.CreatureRegime;
+            //baby.Size = parent.Size*( 1 - mutationFactor/2f + (float)rand.NextDouble() * mutationFactor);
+            //baby.MaxSpeed = parent.MaxSpeed* (1 - mutationFactor / 2f + (float)rand.NextDouble() * mutationFactor);
+            //baby.Sensor = new CircularSensor(parent.Sensor.SensingRadius * (1 - mutationFactor / 2f + (float)rand.NextDouble() * mutationFactor));
+            //baby.MaxEnergy = parent.MaxEnergy;
+            //baby.Generation = parent.Generation + 1;
+            //Evolution Off
             baby.CreatureRegime = parent.CreatureRegime;
-            baby.Size = parent.Size*( 1 - mutationFactor/2f + (float)rand.NextDouble() * mutationFactor);
-            baby.MaxSpeed = parent.MaxSpeed* (1 - mutationFactor / 2f + (float)rand.NextDouble() * mutationFactor);
-            baby.Sensor = new CircularSensor(parent.Sensor.SensingRadius * (1 - mutationFactor / 2f + (float)rand.NextDouble() * mutationFactor));
+            baby.Size = parent.Size;
+            baby.MaxSpeed = parent.MaxSpeed;
+            baby.Sensor = new CircularSensor(parent.Sensor.SensingRadius);
             baby.MaxEnergy = parent.MaxEnergy;
             baby.Generation = parent.Generation + 1;
         }
